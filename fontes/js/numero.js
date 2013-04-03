44f4e94b349f0be213a8dfa029f6bab9aac55de7
Domínio L <lucas@dominiol.com.br>
2013-04-03 00:15:53 -0300

    Adição dos métodos sortear e sortearInteiro para Number e conclusão da tarefa #32.

--- fontes/js/numero.js
+++ fontes/js/numero.js
@@ -6,6 +6,44 @@
 	Number.estender({
 		naoNumero: function (valor) {
 			return (valor !== valor);
+		},
+		
+		sortear: function () {
+			var limiteInferior = Math.min(limiteA, limiteB);
+			var limiteSuperior = Math.max(limiteA, limiteB);
+			return (Math.random() * (limiteSuperior - limiteInferior) + limiteInferior);
+		},
+		
+		sortearInteiro: function (limiteA, limiteB) {
+			var limiteInferior = Math.min(limiteA, limiteB);
+			var limiteSuperior = Math.max(limiteA, limiteB);
+			return (Math.floor(Math.random() * (limiteSuperior - limiteInferior + 1)) + Math.floor(limiteInferior));
+		}
+	});
+	
+	Number.definirPropriedades({
+		maximo: {
+			funcaoFornecer: function () {
+				return Number.MAX_VALUE;
+			}
+		},
+		
+		minimo: {
+			funcaoFornecer: function () {
+				return Number.MIN_VALUE;
+			}
+		},
+		
+		maisInfinito: {
+			funcaoFornecer: function () {
+				return Number.POSITIVE_INFINITY;
+			},
+		},
+		
+		menosInfinito: {
+			funcaoFornecer: function () {
+				return Number.NEGATIVE_INFINITY;
+			}
 		}
 	});
 }());
