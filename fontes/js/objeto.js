72c49e36c20508e554a105f2f1e3a52be9e8095d
Lucas <lucas@dominiol.com.br>
2013-09-19 05:21:48 -0300

    Conclusão da tarefa #25: método paraCada em objetos, implementação de métodos cadaPropriedade, fornecerPropriedades, possuiPropriedades e outros em Object.

--- fontes/js/objeto.js
+++ fontes/js/objeto.js
@@ -5,6 +5,235 @@
 	"use strict";
 
 	Object.implementar({
+		cadaPropriedade: function (iterador, escopo) {
+			iterador = iterador.vincularEscopo(escopo);
+			var propriedades = this.fornecerPropriedades();
+			for (var indice = 0, tamanho = propriedades.length; indice < tamanho; indice++) {
+				var chave = propriedades[indice];
+				iterador(this[chave], chave);
+			}
+		},
+
+		cadaPropriedadeEnumeravel: function (iterador, escopo) {
+			iterador = iterador.vincularEscopo(escopo);
+			for (var chave in this) {
+				iterador(this[chave], chave);
+			}
+		},
+
+		cadaPropriedadeInvisivel: function (iterador, escopo) {
+			iterador = iterador.vincularEscopo(escopo);
+			var propriedadesInvisiveis = this.fornecerPropriedadesInvisiveis();
+			for (var indice = 0, tamanho = propriedadesInvisiveis.length; indice < tamanho; indice++) {
+				var chave = propriedadesInvisiveis[indice];
+				iterador(this[chave], chave);
+			}
+		},
+
+		cadaPropriedadePropria: function (iterador, escopo) {
+			iterador = iterador.vincularEscopo(escopo);
+			var propriedadesProprias = this.fornecerPropriedadesProprias();
+			for (var indice = 0, tamanho = propriedadesProprias.length; indice < tamanho; indice++) {
+				var chave = propriedadesProprias[indice];
+				iterador(this[chave], chave);
+			}
+		},
+
+		cadaPropriedadePropriaEnumeravel: function (iterador, escopo) {
+			iterador = iterador.vincularEscopo(escopo);
+			var propriedadesPropriasEnumeraveis = this.fornecerPropriedadesPropriasEnumeraveis();
+			for (var indice = 0, tamanho = propriedadesPropriasEnumeraveis.length; indice < tamanho; indice++) {
+				var chave = propriedadesPropriasEnumeraveis[indice];
+				iterador(this[chave], chave);
+			}
+		},
+
+		cadaPropriedadePropriaInvisivel: function (iterador, escopo) {
+			iterador = iterador.vincularEscopo(escopo);
+			var propriedadesPropriasInvisiveis = this.fornecerPropriedadesPropriasInvisiveis();
+			for (var indice = 0, tamanho = propriedadesPropriasInvisiveis.length; indice < tamanho; indice++) {
+				var chave = propriedadesPropriasInvisiveis[indice];
+				iterador(this[chave], chave);
+			}
+		},
+
+		cadaPropriedadeHerdada: function (iterador, escopo) {
+			iterador = iterador.vincularEscopo(escopo);
+			var propriedadesHerdadas = this.fornecerPropriedadesHerdadas();
+			for (var indice = 0, tamanho = propriedadesHerdadas.length; indice < tamanho; indice++) {
+				var chave = propriedadesHerdadas[indice];
+				iterador(this[chave], chave);
+			}
+		},
+
+		cadaPropriedadeHerdadaEnumeravel: function (iterador, escopo) {
+			iterador = iterador.bind(escopo);
+			for (var chave in this) {
+				if (!this.possuiPropriedadePropriaEnumeravel(chave)) {
+					iterador(this[chave], chave);
+				}
+			}
+		},
+
+		cadaPropriedadeHerdadaInvisivel: function (iterador, escopo) {
+			iterador = iterador.vincularEscopo(escopo);
+			var propriedadesHerdadasInvisiveis = this.fornecerPropriedadesHerdadasInvisiveis();
+			for (var indice = 0, tamanho = propriedadesHerdadasInvisiveis.length; indice < tamanho; indice++) {
+				var chave = propriedadesHerdadasInvisiveis[indice];
+				iterador(this[chave], chave);
+			}
+		},
+
+		fornecerPropriedades: function () {
+			var propriedades = this.fornecerPropriedadesProprias();
+			var prototipo = this.fornecerPrototipo();
+			while (!Linda.nulo(prototipo)) {
+				propriedades.push.aplicarComEscopo(propriedades, prototipo.fornecerPropriedadesProprias());
+				prototipo = prototipo.fornecerPrototipo();
+			}
+			return propriedades;
+		},
+
+		fornecerPropriedadesEnumeraveis: function () {
+			var propriedadesEnumeraveis = [];
+			for (var propriedade in this) {
+				propriedadesEnumeraveis.push(propriedade);
+			}
+			return propriedadesEnumeraveis;
+		},
+
+		fornecerPropriedadesInvisiveis: function () {
+			var propriedades = this.fornecerPropriedades();
+			var propriedadesInvisiveis = [];
+			for (var indice = 0, tamanho = propriedades.length; indice < tamanho; indice++) {
+				var propriedade = propriedades[indice];
+				if (!this.possuiPropriedadeEnumeravel(propriedade)) {
+					propriedadesInvisiveis.push(propriedade);
+				}
+			}
+			return propriedadesInvisiveis;
+		},
+
+		fornecerPropriedadesProprias: function () {
+			return Object.getOwnPropertyNames(this);
+		},
+
+		fornecerPropriedadesPropriasEnumeraveis: function () {
+			return Object.keys(this);
+		},
+
+		fornecerPropriedadesPropriasInvisiveis: function () {
+			var propriedadesProprias = this.fornecerPropriedadesProprias();
+			var propriedadesPropriasInvisiveis = [];
+			for (var indice = 0, tamanho = propriedadesProprias.length; indice < tamanho; indice++) {
+				var propriedadePropria = propriedadesProprias[indice];
+				if (!this.possuiPropriedadePropriaEnumeravel(propriedadePropria)) {
+					propriedadesPropriasInvisiveis.push(propriedadePropria);
+				}
+			}
+			return propriedadesPropriasInvisiveis;
+		},
+
+		fornecerPropriedadesHerdadas: function () {
+			var propriedadesHerdadas = [];
+			var prototipo = this.fornecerPrototipo();
+			while (!Linda.nulo(prototipo)) {
+				propriedadesHerdadas.push.aplicarComEscopo(propriedadesHerdadas, prototipo.fornecerPropriedadesProprias());
+				prototipo = prototipo.fornecerPrototipo();
+			}
+			return propriedadesHerdadas;
+		},
+
+		fornecerPropriedadesHerdadasEnumeraveis: function () {
+			var propriedadesHerdadasEnumeraveis = []
+			for (var propriedadeEnumeravel in this) {
+				if (!this.possuiPropriedadePropriaEnumeravel(propriedadeEnumeravel)) {
+					propriedadesHerdadasEnumeraveis.push(propriedadeEnumeravel);
+				}
+			}
+			return propriedadesHerdadasEnumeraveis;
+		},
+
+		fornecerPropriedadesHerdadasInvisiveis: function () {
+			var propriedadesHerdadas = this.fornecerPropriedadesHerdadas();
+			var propriedadesHerdadasInvisiveis = [];
+			for (var indice = 0, tamanho = propriedadesHerdadas.length; indice < tamanho; indice++) {
+				var propriedadeHerdada = propriedadesHerdadas[indice];
+				if (!this.possuiPropriedadeHerdadaEnumeravel(propriedadeHerdada)) {
+					propriedadesHerdadasInvisiveis.push(propriedadeHerdada);
+				}
+			}
+			return propriedadesHerdadasInvisiveis;
+		},
+
+		possuiPropriedade: function (propriedade) {
+			return (propriedade in this);
+		},
+
+		possuiPropriedadeEnumeravel: function (propriedade) {
+			for (var minhaPropriedade in this) {
+				if (minhaPropriedade === propriedade) {
+					return true;
+				}
+			}
+			return false;
+		},
+
+		possuiPropriedadeInvisivel: function (propriedade) {
+			return (this.possuiPropriedade(propriedade) && !this.possuiPropriedadeEnumeravel(propriedade));
+		},
+
+		possuiPropriedadePropria: function (propriedade) {
+			return this.hasOwnProperty(propriedade);
+		},
+
+		possuiPropriedadePropriaEnumeravel: function (propriedade) {
+			return this.propertyIsEnumerable(propriedade);
+		},
+
+		possuiPropriedadePropriaInvisivel: function (propriedade) {
+			return (this.possuiPropriedadePropria(propriedade) && !this.possuiPropriedadePropriaEnumeravel(propriedade));
+		},
+
+		possuiPropriedadeHerdada: function (propriedade) {
+			return (this.possuiPropriedade(propriedade) && !this.possuiPropriedadePropria(propriedade));
+		},
+
+		possuiPropriedadeHerdadaEnumeravel: function (propriedade) {
+			return (this.possuiPropriedadeEnumeravel(propriedade) && !this.possuiPropriedadePropriaEnumeravel(propriedade));
+		},
+
+		possuiPropriedadeHerdadaInvisivel: function (propriedade) {
+			return (this.possuiPropriedadeHerdada(propriedade) && !this.possuiPropriedadeHerdadaEnumeravel(propriedade));
+		},
+
+		instanciaDe: function (tipo) {
+			if (!Linda.tipoDe(tipo, Function)) {
+				return false;
+			}
+			return tipo.prototype.prototipoDe(this);
+		},
+
+		prototipadoDe: function (prototipoDoTipo) {
+			var prototipoDoObjeto = this.fornecerPrototipo();
+			while (!Linda.nulo(prototipoDoObjeto) && prototipoDoObjeto !== prototipoDoTipo) {
+				prototipoDoObjeto = prototipoDoObjeto.fornecerPrototipo();
+			}
+			return (prototipoDoObjeto == prototipoDoTipo);
+		},
+
+		prototipoDe: function (objeto) {
+			var prototipoDoObjeto = objeto.fornecerPrototipo();
+			while (!Linda.nulo(prototipoDoObjeto) && prototipoDoObjeto !== this) {
+				prototipoDoObjeto = prototipoDoObjeto.fornecerPrototipo();
+			}
+			return (prototipoDoObjeto === this);
+		},
+
+		fornecerPrototipo: function () {
+			return Object.getPrototypeOf(this);
+		},
+
 		definirPropriedade: function (atributo, definicao) {
 			var propriedades = {};
 			this.privadoDefinirPropriedade(propriedades, "value", definicao.valor);
@@ -27,14 +256,6 @@
 			if (!Linda.indefinido(valor)) {
 				propriedades[chave] = valor;
 			}
-		},
-
-		fornecerPropriedades: function () {
-			return Object.getOwnPropertyNames(this);
-		},
-
-		fornecerPropriedadesEnumeraveis: function () {
-			return Object.keys(this);
 		},
 
 		fundir: function (outro) {
@@ -77,26 +298,9 @@
 		desobservar: function (tratador) {
 			Object.unobserve(this, tratador);
 		},
+	});
 
-		paraCada: function (funcaoDeIteracao, escopo) {
-			funcaoDeIteracao = funcaoDeIteracao.vincularEscopo(escopo);
-			for (var chave in this) {
-				if (this.possuiPropriedadePropria(chave)) {
-					funcaoDeIteracao(this[chave], chave);
-				}
-			}
-		},
-
-		possuiPropriedade: function (propriedade) {
-			return (propriedade in this);
-		},
-
-		possuiPropriedadePropria: function (propriedade) {
-			return this.hasOwnProperty(propriedade);
-		},
-
-		prototipoDe: function (prototipado) {
-			return this.isPrototypeOf(prototipado);
-		}
+	Object.implementar({
+		paraCada: Object.cadaPropriedadePropriaEnumeravel
 	});
 }(this));
