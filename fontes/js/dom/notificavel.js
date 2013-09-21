d96ab2f639e5e8045839126f8a927cfb33b399c5
Lucas <lucas@dominiol.com.br>
2013-09-21 16:58:08 -0300

    Adição da possibilidade de remover tratadores.

--- fontes/js/dom/notificavel.js
+++ fontes/js/dom/notificavel.js
@@ -10,10 +10,18 @@
 			this.elementoDom = elementoDom;
 		},
 
+		deixarDeTratar: function (evento, funcao) {
+			Dom.extrair(this).removeEventListener(evento, funcao);
+		},
+
 		tratarAlteracao: function (tratador, escopo) {
 			tratador = tratador.vincularEscopo(escopo);
 			Dom.extrair(this).addEventListener("change", tratador);
 			return tratador;
+		},
+
+		deixarDeTratarAlteracao: function (funcao) {
+			this.deixarDeTratar("change", funcao);
 		},
 
 		tratarAlteracaoNoHistorico: function (tratador, escopo) {
@@ -22,10 +30,18 @@
 			return tratador;
 		},
 
+		deixarDeTratarAlteracaoNoHistorico: function (funcao) {
+			this.deixarDeTratar("popstate", funcao);
+		},
+
 		tratarErro: function (tratador, escopo) {
 			tratador = tratador.vincularEscopo(escopo);
 			Dom.extrair(this).addEventListener("error", tratador);
 			return tratador;
+		},
+
+		deixarDeTratarErro: function (funcao) {
+			this.deixarDeTratar("error", funcao);
 		},
 
 		tratarCarregamento: function (tratador, escopo) {
@@ -34,10 +50,18 @@
 			return tratador;
 		},
 
+		deixarDeTratarCarregamento: function (funcao) {
+			this.deixarDeTratar("load", funcao);
+		},
+
 		tratarCarregamentoIniciado: function (tratador, escopo) {
 			tratador = tratador.vincularEscopo(escopo);
 			Dom.extrair(this).addEventListener("loadstart", tratador);
 			return tratador;
+		},
+
+		deixarDeTratarCarregamentoIniciado: function (funcao) {
+			this.deixarDeTratar("loadstart", funcao);
 		},
 
 		tratarCarregamentoFinalizado: function (tratador, escopo) {
@@ -46,10 +70,18 @@
 			return tratador;
 		},
 
+		deixarDeTratarCarregamentoFinalizado: function (funcao) {
+			this.deixarDeTratar("loadend", funcao);
+		},
+
 		tratarProgresso: function (tratador, escopo) {
 			tratador = tratador.vincularEscopo(escopo);
 			Dom.extrair(this).addEventListener("progress", tratador);
 			return tratador;
+		},
+
+		deixarDeTratarProgresso: function (funcao) {
+			this.deixarDeTratar("progress", funcao);
 		},
 
 		tratarAborto: function (tratador, escopo) {
@@ -58,10 +90,18 @@
 			return tratador;
 		},
 
+		deixarDeTratarAborto: function (funcao) {
+			this.deixarDeTratar("abort", funcao);
+		},
+
 		tratarEstouroDeTempo: function (tratador, escopo) {
 			tratador = tratador.vincularEscopo(escopo);
 			Dom.extrair(this).addEventListener("timeout", tratador);
 			return tratador;
+		},
+
+		deixarDeTratarEstouroDeTempo: function (funcao) {
+			this.deixarDeTratar("timeout", funcao);
 		},
 
 		tratarClique: function (tratador, escopo) {
@@ -70,10 +110,18 @@
 			return tratador;
 		},
 
+		deixarDeTratarClique: function (funcao) {
+			this.deixarDeTratar("click", funcao);
+		},
+
 		tratarCliqueDuplo: function (tratador, escopo) {
 			tratador = tratador.vincularEscopo(escopo);
 			Dom.extrair(this).addEventListener("dbclick", tratador);
 			return tratador;
+		},
+
+		deixarDeTratarCliqueDuplo: function (funcao) {
+			this.deixarDeTratar("dbclick", funcao);
 		},
 
 		tratarTeclaPressionada: function (tecla, tratador, escopo) {
@@ -86,6 +134,10 @@
 			return tratadorPersonalizado;
 		},
 
+		deixarDeTratarTeclaPressionada: function (funcao) {
+			this.deixarDeTratar("keydown", funcao);
+		},
+
 		tratarTeclaSolta: function (tecla, tratador, escopo) {
 			var tratadorPersonalizado = function (evento) {
 				if (tecla === evento.keyCode) {
@@ -94,6 +146,10 @@
 			};
 			Dom.extrair(this).addEventListener("keyup", tratadorPersonalizado);
 			return tratadorPersonalizado;
+		},
+
+		deixarDeTratarTeclaSolta: function (funcao) {
+			this.deixarDeTratar("keyup", funcao);
 		},
 
 		tratarQualquerTeclaPressionada: function (tratador, escopo) {
